c0bc5b5fb6c43786b58f29646b2032841a2827ba
fusheng <2812313382@qq.com>
2020-11-02 06:57:59 +0800

    模型菜单功能开始编写

---
+++ app/admin/controller/GoodsType.php
@@ -0,0 +1,13 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\admin\controller;
+
+class GoodsType extends Base
+{
+    // 模型列表
+    public function index()
+    {
+        return view();
+    }
+}
